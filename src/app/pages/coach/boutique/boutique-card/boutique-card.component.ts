e89c952d602a8540a2e1d7ab34ebe1a91d82eddf
Ahmed Nouira <ahmnouira@gmail.com>
2022-08-17 14:41:10 +0100

    fixed boutique module

--- src/app/pages/coach/boutique/boutique-card/boutique-card.component.ts
+++ src/app/pages/coach/boutique/boutique-card/boutique-card.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Formation } from 'src/app/models/formation/formation.model';
 
 @Component({
-  selector: 'app-boutique',
+  selector: 'app-boutique-card',
   templateUrl: './boutique-card.component.html',
   styleUrls: ['./boutique-card.component.scss'],
 })
 export class BoutiqueCardComponent implements OnInit {
+
+  @Input() formation : Formation
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/pages/coach/boutique/boutique.module.ts
+++ src/app/pages/coach/boutique/boutique.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoutiqueRoutingModule } from './boutique-routing.module';
-import { CcListComponent } from '../coach-client-management/cc-list/cc-list.component';
-import { CcAddComponent } from '../coach-client-management/cc-add/cc-add.component';
 import { ComponentsModule } from '../../components/components.module';
 import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BoutiqueListComponent } from './boutique-list/boutique-list.component';
+import { BoutiqueCardComponent} from './boutique-card/boutique-card.component'
 
 @NgModule({
-  declarations: [CcListComponent, CcAddComponent, BoutiqueListComponent],
+  declarations: [BoutiqueCardComponent, BoutiqueListComponent],
   imports: [
     CommonModule,
     BoutiqueRoutingModule,
