d148e09088f8f8fe04ed0b4d6691f937ffbda343
Ahmed Nouira <ahmnouira@gmail.com>
2022-08-18 16:54:08 +0100

    added fixes

--- src/app/pages/coach/plans/plans.component.ts
+++ src/app/pages/coach/plans/plans.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { PLANS } from 'src/app/constants/plans';
 import { Plan } from 'src/app/models/plan.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-plans',
@@ -15,9 +17,55 @@ export class PlansComponent implements OnInit {
   // monthly by default
   monthly = true;
 
-  constructor() {}
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
+
+  initStripe(plan: Plan) {
+      const scriptScript = this.document.getElementById('stripe-script')
+      if(scriptScript) {
+        scriptScript.remove()
+        setTimeout(() => {}, 1000)
+      }
+
+      let script = this.renderer.createElement('script') as HTMLScriptElement;
+      // v2
+      // script.src = "https://checkout.stripe.com/v2/checkout.js"
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.id = 'stripe-script'
+      script.type = 'application/javascript';
+
+      this.renderer.addClass(script, 'stripe-button');
+      this.renderer.setAttribute(
+        script,
+        'data-key',
+        environment.STRIPE_PUBLISHABLE_KEY
+      );
+      
+
+      console.log('initStripe Runs with', plan.price)
+
+      this.renderer.setAttribute(script, 'data-amount', plan.price);
+      this.renderer.setAttribute(script, 'data-name', environment.APP_NAME);
+      this.renderer.setAttribute(script, 'data-description', plan.title);
+      this.renderer.setAttribute(script, 'data-locale', 'auto');
+      this.renderer.appendChild(
+        this.document.getElementById('stripeForm'),
+        script
+      );
+
+      setTimeout(() => {
+        const button = this.document.getElementsByClassName(
+          'stripe-button-el'
+        )[0] as HTMLButtonElement;
+        button.style.display = 'none';
+      }, 500);
+}
 
   changeOffer(event: any) {
     this.monthly = !this.monthly;
@@ -34,6 +82,8 @@ export class PlansComponent implements OnInit {
 
   setSelectedPlan(plan: Plan) {
     this.selectedPlan = plan;
+    console.log('selectedPlan', plan)
+    this.initStripe(plan)
    
   }
 }
