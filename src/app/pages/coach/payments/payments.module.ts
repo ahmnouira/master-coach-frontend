ff40c89a74bc9d11163bd29d3a60afa607aba208
Ahmed Nouira <ahmnouira@gmail.com>
2022-09-01 16:23:20 +0100

    added payments page

--- src/app/pages/coach/payments/payments.module.ts
+++ src/app/pages/coach/payments/payments.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { PaymentsRoutingModule } from './payments-routing.module';
 import { PaymentsListComponent } from './payments-list/payments-list.component';
+import { LoadingCardModule } from 'src/app/shared/loading-card/loading-card.module';
+import { DatatableModule } from 'src/app/shared/datatable/datatable.module';
+import { ComponentsModule } from '../../components/components.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PaymentsListComponent],
-  imports: [CommonModule, PaymentsRoutingModule],
+  imports: [CommonModule, PaymentsRoutingModule, 
+    SharedModule,
+    ComponentsModule,
+    LoadingCardModule,
+    DatatableModule,
+    FormsModule
+  ],
 })
 export class PaymentsModule {}
