38499c9183fb8ea88e6a8631e3276be8b16c2358
Ahmed Nouira <ahmnouira@gmail.com>
2022-09-05 09:34:38 +0100

    generate items

---
+++ src/app/pages/cart/cart-card/cart-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CartCardComponent } from './cart-card.component';
+
+describe('CartCardComponent', () => {
+  let component: CartCardComponent;
+  let fixture: ComponentFixture<CartCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CartCardComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CartCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
